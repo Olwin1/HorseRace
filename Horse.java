37a63b312d7a2d3317e79f5f53e312a2b78e40a0
Olwin1 <oliver_munn@icloud.com>
2025-03-26 14:01:31 +0000

    Added hasFallen and distanceTravelled with corresponding implementations for the getters and setters

--- Horse.java
+++ Horse.java
@@ -11,6 +11,8 @@ public class Horse
     /////////////////////////////////////////
     // Instance variables for horse class. //
     /////////////////////////////////////////
+    
+    // Constructor-initialised fields
 
     /// Used to determine how the horse is represented on the screen. 
     private char horseSymbol;
@@ -24,6 +26,15 @@ public class Horse
     /// - A lower confidence means the horse runs slower but is more stable.  
 
     private double horseConfidence;
+
+    // Implicitly initialised fields
+
+    /// A field that determines weather or not the horse has fallen.  
+    private boolean hasFallen = false;
+
+    /// A field that is used to store the horse's progress during the race.  
+    private int distanceTravelled = 0;
+
 
     /////////////////////////////////////
     // Horse class method definitions. //
@@ -40,6 +51,7 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
+        // Set the provided arguments to the corresponding instance variables
         this.horseSymbol = horseSymbol;
         this.horseConfidence = horseConfidence;
         this.horseName = horseName;
@@ -48,55 +60,104 @@ public class Horse
     
     
     
-    //Other methods of class Horse
+    //Generic methods of class Horse
+
+    /// Sets the [hasFallen] flag to true.  
     public void fall()
     {
-        
+        this.hasFallen = true;
     }
     
+    /**
+     * Gets the [horseConfidence] field. 
+     *  
+     * A higher confidence means the horse runs faster but is also more prone to falling.  
+     * A lower confidence means the horse runs slower but is more stable.  
+     * 
+     * @return The horse confidence.  
+     */
     public double getConfidence()
     {
         return this.horseConfidence;
     }
     
+    /**
+     * Gets the total distance the horse has travelled throughout the race.  
+     * @return The distance travelled.
+     */
     public int getDistanceTravelled()
     {
-        
+        return this.distanceTravelled;
     }
     
+    /**
+     * Gets the horse's name.  
+     * @return The horse's name.  
+     */
     public String getName()
     {
         return this.horseName;
     }
     
+    /**
+     * Gets the symbol that is used to represent the horse during the race.  
+     * @return The horse's racing symbol.  
+     */
     public char getSymbol()
     {
         return this.horseSymbol;
     }
     
+    /**
+     * Resets the horse's position in the race,
+     * sending them back to the start by setting the [distanceTravelled] to 0
+     * This will also reset their [hasFallen] flag.  
+     */
     public void goBackToStart()
     {
-        
+        this.distanceTravelled = 0;
+        this.hasFallen = false;
     }
     
+    /**
+     * Gets the flag that determines if the horse has fallen or not.  
+     * @return A boolean [hasFallen] indicator.  
+     */
     public boolean hasFallen()
     {
-        
+        return this.hasFallen;
     }
 
+    /**
+     * Moves the horse forwards 1 by 
+     * incrementing [distanceTravelled] by 1.   
+     */
     public void moveForward()
     {
-        
+        this.distanceTravelled += 1;
     }
 
+    /**
+     * Setter to set a new horse confidence.  
+     * @param newConfidence 
+     * Must be a double between 0 and 1.  
+     * A higher confidence means the horse runs faster but is also more prone to falling.  
+     * A lower confidence means the horse runs slower but is more stable.  
+     */
     public void setConfidence(double newConfidence)
     {
-        
+        this.horseConfidence = newConfidence;
     }
     
+    /**
+     * Setter to set a new horse symbol.  
+     * This is used to change the symbol that is used to represent the horse during the race.  
+     * 
+     * @param newSymbol A character parameter that will replace the existing horse symbol on the screen.  
+     */
     public void setSymbol(char newSymbol)
     {
-        
+        this.horseSymbol = newSymbol;
     }
     
 }
